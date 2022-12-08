f4d766d3fe8274f12a8ca2770ad4ffe7c68231b4
Armadillan <74256936+Armadillan@users.noreply.github.com>
2022-12-08 13:52:05 +0100

    python int pointer hack test

--- powarmup23/test.py
+++ powarmup23/test.py
@@ -1,1 +1,21 @@
-a = "abcdefg"
+# from dataclasses import dataclass
+
+# @dataclass
+# class WeirdInt:
+#     val: int
+
+
+# a = [WeirdInt(x) for x in range(7)]
+
+# a[2] = a[3]
+# a[1] = a[2]
+# a[3].val = 1000
+# print(a)
+
+
+# a = 1
+# b = 2
+# c = 3
+
+# a, b, c = map(int, (a, b, c))
+# a,b,c = (int(x) for x in (a,b,c))
